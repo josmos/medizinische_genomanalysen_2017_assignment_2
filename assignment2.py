9aea068f7a05891a821153f26140fe14ad81dba6
josmos <josmos43@gmail.com>
2017-03-21 14:43:41 +0100

    FIN!

--- assignment2.py
+++ assignment2.py
@@ -3,6 +3,7 @@
 import vcf
 import numpy as np
 import os
+import subprocess
 
 __author__ = 'Josef Moser'
 
@@ -10,9 +11,12 @@ __author__ = 'Josef Moser'
 class Assignment2:
     
     def __init__(self):
-        ## Check if pyvcf is installed
-        print("PyVCF version: %s" % vcf.VERSION)
+        if not os.path.isfile("AmpliseqExome.20141120.NA24385.vcf"):
+            subprocess.call(["wget", "ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/"
+                                     "AshkenazimTrio/analysis/IonTorrent_TVC_03162015/"
+                                     "AmpliseqExome.20141120.NA24385.vcf"])
         self.vcf = vcf.Reader(filename="AmpliseqExome.20141120.NA24385.vcf")
+        self.header = self.vcf._header_lines
         self.records = list([i for i in self.vcf])
 
     def get_average_quality_of_son(self):
@@ -21,7 +25,7 @@ class Assignment2:
         :return: 
         '''
         s = np.mean([i.QUAL for i in self.records])
-        print("Average PHRED Score is {}".format(s))
+        print("\nAverage PHRED Score is {}".format(s))
 
     def get_total_number_of_variants_of_son(self):
         '''
@@ -29,29 +33,24 @@ class Assignment2:
         :return: total number of variants
         '''
         sum = len(self.records)
-        print("Number of Variants {}".format(sum))
-    
-    
+        print("\nNumber of Variants {}".format(sum))
+
     def get_variant_caller_of_vcf(self):
         '''
         Return the variant caller name
         :return: 
         '''
-        source = "##source="
-        var_caller = [h.lstrip(source) for h in self.vcf._header_lines
-                      if h.startswith(source)][0]
-        print("Variant Caller: {}".format(var_caller.strip('"')))
+        var_caller = [h.lstrip("##source=") for h in self.header if h.startswith("##source=")][0]
+        print("\nVariant Caller: {}".format(var_caller.strip('"')))
 
     def get_human_reference_version(self):
         '''
         Return the genome reference version
         :return: 
         '''
-        ref = "##reference="
-        path = [h.lstrip(ref) for h in self.vcf._header_lines
-                if h.startswith(ref)][0]
+        path = [h.lstrip("##reference=") for h in self.header if h.startswith("##reference=")][0]
         version = os.path.basename(path).strip(".fasta")
-        print("Reference Version: {}".format(version))
+        print("\nReference Version: {}".format(version))
         
     def get_number_of_indels(self):
         '''
@@ -59,7 +58,7 @@ class Assignment2:
         :return: 
         '''
         indel_cnt = len([i for i in self.records if i.is_indel])
-        print("Number of indels: {}".format(indel_cnt))
+        print("\nNumber of indels: {}".format(indel_cnt))
 
     def get_number_of_snvs(self):
         '''
@@ -67,7 +66,7 @@ class Assignment2:
         :return: 
         '''
         snp_cnt = len([i for i in self.records if i.is_snp])
-        print("Number of SNVs: {}".format(snp_cnt))
+        print("\nNumber of SNVs: {}".format(snp_cnt))
         
     def get_number_of_heterozygous_variants(self):
         '''
@@ -75,19 +74,19 @@ class Assignment2:
         :return: 
         TOTO
         '''
-
-        
+        het = [i for i in self.records if i.num_het == 1]
+        print("\nNumber of heterozygous variants: {}".format(len(het)))
     
     def print_summary(self):
-        # self.get_average_quality_of_son()
-        # self.get_total_number_of_variants_of_son()
-        # self.get_variant_caller_of_vcf()
-        # self.get_human_reference_version()
-        # self.get_number_of_indels()
-        # self.get_number_of_snvs()
+        print(__author__)
+        self.get_average_quality_of_son()
+        self.get_total_number_of_variants_of_son()
+        self.get_variant_caller_of_vcf()
+        self.get_human_reference_version()
+        self.get_number_of_indels()
+        self.get_number_of_snvs()
         self.get_number_of_heterozygous_variants()
 
-        
 if __name__ == '__main__':
     print("Assignment 2")
     assignment1 = Assignment2()
