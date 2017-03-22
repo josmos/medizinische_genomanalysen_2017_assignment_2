e3fe70e41de66bf397be71eb911ee56946925dda
josmos <josmos43@gmail.com>
2017-03-22 08:30:45 +0100

    faster with cyvcf2

--- assignment2.py
+++ assignment2.py
@@ -1,72 +1,71 @@
 #! /usr/bin/env python3
 
-import vcf
 import numpy as np
 import os
 import subprocess
+from cyvcf2 import VCF
+import vcf
 
 __author__ = 'Josef Moser'
 
 
 class Assignment2:
-    
     def __init__(self):
         if not os.path.isfile("AmpliseqExome.20141120.NA24385.vcf"):
             subprocess.call(["wget", "ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/"
                                      "AshkenazimTrio/analysis/IonTorrent_TVC_03162015/"
                                      "AmpliseqExome.20141120.NA24385.vcf"])
-        self.vcf = vcf.Reader(filename="AmpliseqExome.20141120.NA24385.vcf")
-        self.header = self.vcf._header_lines
-        self.records = list([i for i in self.vcf])
+        self.vcf = "AmpliseqExome.20141120.NA24385.vcf"
+        self.metadata = vcf.Reader(filename=self.vcf).metadata
 
     def get_average_quality_of_son(self):
         '''
         Get the average PHRED quality of all variants
-        :return: 
+        :return:
         '''
-        s = np.mean([i.QUAL for i in self.records])
-        print("\nAverage PHRED Score is {}".format(s))
+        print("\nAverage PHRED Score is {}"
+              .format(np.mean([i.QUAL for i in VCF(self.vcf)])))
 
     def get_total_number_of_variants_of_son(self):
         '''
         Get the total number of variants
         :return: total number of variants
         '''
-        sum = len(self.records)
-        print("\nNumber of Variants {}".format(sum))
+        print("\nNumber of Variants {}"
+              .format(len([i for i in VCF(self.vcf)])))
 
     def get_variant_caller_of_vcf(self):
         '''
         Return the variant caller name
-        :return: 
+        :return:
         '''
-        var_caller = [h.lstrip("##source=") for h in self.header if h.startswith("##source=")][0]
-        print("\nVariant Caller: {}".format(var_caller.strip('"')))
+        print("\nVariant Caller: {}"
+              .format(self.metadata["source"]))
+
 
     def get_human_reference_version(self):
         '''
         Return the genome reference version
-        :return: 
+        :return:
         '''
-        path = [h.lstrip("##reference=") for h in self.header if h.startswith("##reference=")][0]
-        version = os.path.basename(path).strip(".fasta")
-        print("\nReference Version: {}".format(version))
+        print("\nReference Version: {}"
+              .format(os.path.basename(self.metadata["reference"]).strip(".fasta")))
         
     def get_number_of_indels(self):
         '''
         Return the number of identified INDELs 
         :return: 
         '''
-        indel_cnt = len([i for i in self.records if i.is_indel])
-        print("\nNumber of indels: {}".format(indel_cnt))
+        print("\nNumber of indels: {}"
+              .format(len([i for i in VCF(self.vcf) if i.is_indel])))
 
     def get_number_of_snvs(self):
         '''
         Return the number of SNVs
         :return: 
         '''
-        snp_cnt = len([i for i in self.records if i.is_snp])
-        print("\nNumber of SNVs: {}".format(snp_cnt))
+        print("\nNumber of SNVs: {}"
+              .format(len([i for i in VCF(self.vcf) if i.is_snp])))
         
     def get_number_of_heterozygous_variants(self):
         '''
@@ -74,8 +73,8 @@ class Assignment2:
         :return: 
         TOTO
         '''
-        het = [i for i in self.records if i.num_het == 1]
-        print("\nNumber of heterozygous variants: {}".format(len(het)))
+        print("\nNumber of heterozygous variants: {}"
+              .format(len([i for i in VCF(self.vcf) if i.num_het == 1])))
     
     def print_summary(self):
         print(__author__)
